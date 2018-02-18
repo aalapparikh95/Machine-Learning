0ac8f1ec28afe5d8391d6c7d3fa5d9975b16b458
Aalap Parikh <aalap@Aalaps-MacBook-Pro.local>
2018-02-17 19:57:20 -0500

    Calculated mean and std

--- GaussianNB/GNB.py
+++ GaussianNB/GNB.py
@@ -1,4 +1,5 @@
 import math
+import operator
 
 def readFile(filePath):
 	with open(filePath) as f:
@@ -18,6 +19,31 @@ def GaussianNB(data):
 	p['2'] = resClass.count('2')/200
 
 	# Calculate the mean and variances for each attribute corresponding to each class.
+	data2 = list()
+	data1 = list()
+
+	#Divide the data into two classes:
+	for i in data:
+		if i[10] == '1':
+			data1.append(i)
+		elif i[10] == '2':
+			data2.append(i)
+
+	#Take means of both classes
+	sumCol1 = [sum(eval(row[i]) for row in data1) for i in range(1, len(data1[0]) - 1)]
+	meanList1 = [x / 200 for x in sumCol1]
+
+	sumCol2 = [sum(eval(row[i]) for row in data2) for i in range(1, len(data2[0]) - 1)]
+	meanList2 = [x / 200 for x in sumCol2]
+
+	dataAttributes = [i[1:-1] for i in data]
+
+	stdDev1 = [sum([pow(map(operator.sub, i, meanList1), 2) for i in dataAttributes])]
+
+	#variance = dict()
+
+
+
 
 
 
